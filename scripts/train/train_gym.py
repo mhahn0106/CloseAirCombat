747affab72e069f37aa5b88c32fb088416f5f8db
jyh <1579823519@qq.com>
2022-02-24 16:40:36 +0800

    Add gym_compete env

--- scripts/train/train_gym.py
+++ scripts/train/train_gym.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import imp
 import sys
 import os
+from typing import Tuple
 import gym
 import wandb
 import socket
@@ -12,34 +14,41 @@ import logging
 import setproctitle
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from config import get_config
-from runner.jsbsim_runner import JSBSimRunner as Runner
+from runner.gym_runner import GymRunner as Runner
+from envs import gym_compete
 from envs.env_wrappers import SubprocVecEnv, DummyVecEnv
 
 
 class GymEnv:
     def __init__(self, env):
         self.env = env
-        self.action_shape = self.env.action_space.shape
-        self.action_space = self.env.action_space
-        self.observation_space = self.env.observation_space
+        self.num_agents = self.env.num_agents
+        self.action_space = self.env.action_space[0] if isinstance(self.env.action_space, gym.spaces.Tuple) \
+                                                        else self.env.action_space 
+        self.observation_space = self.env.observation_space[0] if isinstance(self.env.observation_space, gym.spaces.Tuple) \
+                                                        else self.env.observation_space 
+        self.obs_shape = (self.num_agents, *self.observation_space.shape)
+        self.act_shape = (self.num_agents, *self.action_space.shape)
+        self.rew_shape = (self.num_agents, 1)
+        self.done_shape = (self.num_agents, 1)
 
     def reset(self):
         observation = self.env.reset()
-        return np.array(observation).reshape((1, -1))
+        return np.array(observation).reshape(self.obs_shape)
 
     def step(self, action):
-        action = np.array(action).reshape(self.action_shape)
+        action = np.array(action).reshape(self.act_shape)
         observation, reward, done, info = self.env.step(action)
-        observation = np.array(observation).reshape((1, -1))
-        done = np.array(done).reshape((1,-1))
-        reward = np.array(reward).reshape((1, -1))
+        observation = np.array(observation).reshape(self.obs_shape)
+        done = np.array(done).reshape(self.done_shape)
+        reward = np.array(reward).reshape(self.rew_shape)
         return observation, reward, done, info
 
     def render(self, mode="human"):
         self.env.render(mode)
 
     def close(self):
-        pass
+        self.env.close()
 
 
 def make_train_env(all_args):
--- test_env.py
+++ test_env.py
@@ -1,32 +1,51 @@
+import imp
 import numpy as np
-from envs.JSBSim.envs import SingleCombatEnv, SingleControlEnv, MultipleCombatEnv
+from envs import gym_compete
 from envs.env_wrappers import SubprocVecEnv, DummyVecEnv
 from envs.JSBSim.core.catalog import Catalog as c
 import logging
+import gym
+from time import sleep
 
-logging.basicConfig(level=logging.DEBUG)
+class GymEnv:
+    def __init__(self, env):
+        self.env = env
+        self.num_agents = self.env.num_agents
+        self.action_space = self.env.action_space[0] if isinstance(self.env.action_space, gym.spaces.Tuple) \
+                                                        else self.env.action_space 
+        self.observation_space = self.env.observation_space[0] if isinstance(self.env.observation_space, gym.spaces.Tuple) \
+                                                        else self.env.observation_space 
+        self.obs_shape = (self.num_agents, *self.observation_space.shape)
+        self.act_shape = (self.num_agents, *self.action_space.shape)
+        self.rew_shape = (self.num_agents, 1)
+        self.done_shape = (self.num_agents, 1)
+
+    def reset(self):
+        observation = self.env.reset()
+        return np.array(observation).reshape(self.obs_shape)
+
+    def step(self, action):
+        action = np.array(action).reshape(self.act_shape)
+        observation, reward, done, info = self.env.step(action)
+        observation = np.array(observation).reshape(self.obs_shape)
+        done = np.array(done).reshape(self.done_shape)
+        reward = np.array(reward).reshape(self.rew_shape)
+        return observation, reward, done, info
+
+    def render(self, mode="human"):
+        self.env.render(mode)
+
+    def close(self):
+        self.env.close()
+
 parallel_num = 1
-envs = DummyVecEnv([lambda: SingleCombatEnv("1v1/ShootMissile/HierarchyVsBaseline") for _ in range(parallel_num)])
+env = gym.make('sumo-ants-v0')
+env = GymEnv(env)
+env = DummyVecEnv([lambda : GymEnv(env) for _ in range(parallel_num)])
 
-# DataType test
-obs_shape = (parallel_num, envs.num_agents, *envs.observation_space.shape)
-act_shape = (parallel_num, envs.num_agents, *envs.action_space.shape)
-reward_shape = (parallel_num, envs.num_agents, 1)
-done_shape = (parallel_num, envs.num_agents, 1)
-
-obss = envs.reset()
-envs.render(mode='txt', filepath='JSBSimRecording.txt.acmi')
-assert obss.shape == obs_shape
-
-actions = np.array([[envs.action_space.sample() for _ in range(envs.num_agents)] for _ in range(parallel_num)])
-print(actions)
+obs = env.reset()
+actions = np.array([[env.action_space.sample() for _ in range(env.num_agents)] for _ in range(parallel_num)])
 while True:
-    obss, rewards, dones, infos = envs.step(actions)
-    envs.render(mode='txt', filepath='JSBSimRecording.txt.acmi')
-    assert obss.shape == obs_shape and actions.shape == act_shape \
-        and rewards.shape == reward_shape and dones.shape == done_shape \
-        and infos.shape[0] == parallel_num and isinstance(infos[0], dict)
-    # terminate if any of the parallel envs has been done
-    if np.any(dones):
+    obs, reward, done, info = env.step(actions)
+    if np.all(done):
         break
-envs.close()
